441d2733ea3655cf5b69f25fb0786ac69f858aa6
fabioCalcagno <39445970+fabioCalcagno@users.noreply.github.com>
2019-12-30 17:38:01 -0300

    first commit

---
+++ diplomas.js
@@ -0,0 +1,58 @@
+
+
+var descendentes = document.querySelectorAll(".cursos-list-itens");
+for (let i = 0; i < descendentes.length; i++) {
+    descendentes[i].addEventListener("click", function (e) {
+      
+        this.setAttribute('class', 'cursos-list-itens-huge'); 
+       
+})
+}
+
+
+var descendentes = document.querySelectorAll(".cursos-list-itens");
+for (let i = 0; i < descendentes.length; i++) {
+    descendentes[i].addEventListener("mousemove", function (e) {
+      
+        this.removeAttribute('class', 'cursos-list-itens-huge');
+        this.setAttribute('class', 'cursos-list-itens');
+       
+})
+}
+    
+function removeHugeImage(hugeImage){
+
+    hugeImage.removeAttribute('class', 'cursos-list-itens-huge');
+    hugeImage.setAttribute('class', 'cursos-list-itens');
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* document.querySelector('cursos-list-itens').onclick = function(event) {
+    event.target.style.backgroundImage = document.documentElement.classList.add('cursos-list-itens-huge');
+};
+
+document.querySelector('.menu-fechar').onclick = function() {
+    document.documentElement.classList.remove('menu-ativo');
+};
+
+
+document.documentElement.onclick = function(event) {
+    if (event.target === document.documentElement) {
+        document.documentElement.classList.remove('menu-ativo');
+    }
+}; */
